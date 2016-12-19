804b3d26b0f573379997473a7b9724e01d00f550
Amos Wenger <amos@itch.io>
2016-12-19 17:51:31 +0100

    Almost a pretty good installer UI

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-	"strings"
+	"os"
+	"path/filepath"
 	"time"
 	"unsafe"
 
@@ -19,6 +21,15 @@ func rectangleFromRECT(r win.RECT) walk.Rectangle {
 		Width:  int(r.Right - r.Left),
 		Height: int(r.Bottom - r.Top),
 	}
+}
+
+func loadImage(filePath string) walk.Image {
+	img, err := walk.NewImageFromFile(filePath)
+	if err != nil {
+		log.Printf("Couldn't load %s: %s\n", filePath, err.Error())
+		return nil
+	}
+	return img
 }
 
 func centerWindow(mw *walk.MainWindow) {
@@ -42,56 +53,147 @@ func centerWindow(mw *walk.MainWindow) {
 }
 
 func main() {
-	var inTE, outTE *walk.TextEdit
+	var outLabel *walk.LineEdit
 	var pb *walk.ProgressBar
 	var progressLabel *walk.Label
 	var mw *walk.MainWindow
+	var imageView *walk.ImageView
+
+	var progressComposite, optionsComposite *walk.Composite
+
+	pickInstallLocation := func() {
+		dlg := new(walk.FileDialog)
+
+		dlg.Title = "Select an install folder"
+
+		if ok, err := dlg.ShowBrowseFolder(mw); err != nil {
+			log.Println(fmt.Sprintf("ShowBrowserFolder error: %s", err.Error()))
+		} else if !ok {
+			// nothing picked
+		} else {
+			outLabel.SetText(dlg.FilePath)
+		}
+	}
+
+	// imageScaleFactor := 0.6
+	// imageWidth := int(1040.0 * imageScaleFactor)
+	// imageHeight := int(673.0 * imageScaleFactor)
+
+	imageWidth := 624
+	imageHeight := 404
+
+	controlsHeight := 120
+
+	windowSize := ui.Size{
+		Width: imageWidth,
+		// Height: imageHeight + controlsHeight,
+		Height: 562,
+	}
 
 	err := ui.MainWindow{
-		Title: "itch Setup",
-		MinSize: ui.Size{
-			Width:  600,
-			Height: 400,
+		Title:   "itch Setup",
+		MinSize: windowSize,
+		MaxSize: windowSize,
+		Size:    windowSize,
+		Layout: ui.VBox{
+			MarginsZero: true,
+			SpacingZero: true,
 		},
-		Layout: ui.VBox{},
 		Children: []ui.Widget{
-			ui.HSplitter{
+			ui.ImageView{
+				AssignTo: &imageView,
+				MinSize:  ui.Size{Width: imageWidth, Height: imageHeight},
+				MaxSize:  ui.Size{Width: imageWidth, Height: imageHeight},
+			},
+			ui.Composite{
+				MinSize: ui.Size{Height: controlsHeight},
+				Layout: ui.HBox{
+					Margins: ui.Margins{
+						Left:  30,
+						Right: 30,
+					},
+				},
 				Children: []ui.Widget{
-					ui.TextEdit{AssignTo: &inTE},
-					ui.TextEdit{AssignTo: &outTE, ReadOnly: true},
-				},
-			},
-			ui.PushButton{
-				Text: "SCREAM",
-				OnClicked: func() {
-					outTE.SetText(strings.ToUpper(inTE.Text()))
-					dlg := new(walk.FileDialog)
+					ui.LineEdit{
+						AssignTo:    &outLabel,
+						Text:        filepath.Join(os.Getenv("LOCALAPPDATA"), "itch"),
+						ToolTipText: "Click to change the install location",
+						OnMouseUp: func(x, y int, button walk.MouseButton) {
+							pickInstallLocation()
+						},
+					},
+					ui.PushButton{
+						MaxSize: ui.Size{Width: 1},
+						Text:    "Install now",
+						OnClicked: func() {
+							progressComposite.SetVisible(true)
+							optionsComposite.SetVisible(false)
 
-					dlg.Title = "Select an install folder"
+							go func() {
+								progress := 0
+								for {
+									pb.SetValue(progress)
+									progressLabel.SetText(fmt.Sprintf("Progress: %d%%", progress))
+									time.Sleep(300 * time.Millisecond)
+									progress = progress + 25
+									if progress > 100 {
+										go func() {
+											dlg, err := walk.NewDialog(mw)
+											if err != nil {
+												panic(err)
+											}
 
-					if ok, err := dlg.ShowBrowseFolder(mw); err != nil {
-						outTE.SetText(fmt.Sprintf("Error: %s", err.Error()))
-					} else if !ok {
-						outTE.SetText("Nothing picked")
-					} else {
-						outTE.SetText(fmt.Sprintf("Path picked: %s", dlg.FilePath))
-					}
+											dlg.SetTitle("Something wrong happened")
+											dlg.Show()
+										}()
+										return
+									}
+								}
+							}()
+						},
+					},
 				},
+				AssignTo: &optionsComposite,
 			},
-			ui.Label{
-				AssignTo: &progressLabel,
-			},
-			ui.ProgressBar{
-				AssignTo: &pb,
+			ui.Composite{
+				MinSize: ui.Size{Height: controlsHeight},
+				Layout: ui.VBox{
+					Margins: ui.Margins{
+						Left:  30,
+						Right: 30,
+					},
+				},
+				Children: []ui.Widget{
+					ui.VSpacer{},
+					ui.ProgressBar{
+						AssignTo: &pb,
+					},
+					ui.VSpacer{Size: 10},
+					ui.Label{
+						Text:     "Starting installation...",
+						AssignTo: &progressLabel,
+					},
+					ui.VSpacer{},
+				},
+				Visible:  false,
+				AssignTo: &progressComposite,
 			},
 		},
 		AssignTo: &mw,
+		OnSizeChanged: func() {
+			if mw == nil {
+				return
+			}
+			// this is kinda crap UX, but resizing the window is really ugly
+			mw.SetSize(walk.Size{Width: windowSize.Width, Height: windowSize.Height})
+		},
 	}.Create()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// see itchSetup.rc
 	ic, err := walk.NewIconFromResourceId(101)
 	if err != nil {
 		log.Println("Could not load icon, oh well")
@@ -99,17 +201,42 @@ func main() {
 		mw.SetIcon(ic)
 	}
 
-	centerWindow(mw)
-
-	go func() {
-		progress := 0
-		for {
-			progress = (progress + 1) % 100
-			pb.SetValue(progress)
-			time.Sleep(60 * time.Millisecond)
-			progressLabel.SetText(fmt.Sprintf("Progress: %d%%", progress))
+	// thanks, go-bindata!
+	func() {
+		data, err := dataInstallerPngBytes()
+		if err != nil {
+			log.Printf("Installer image not found :()")
+			return
 		}
+
+		tf, err := ioutil.TempFile("", "img")
+		if err != nil {
+			log.Printf("Could not create temp file for installer image")
+			return
+		}
+		defer os.Remove(tf.Name())
+
+		_, err = tf.Write(data)
+		if err != nil {
+			log.Printf("Could not write installer image to temp file")
+			return
+		}
+
+		err = tf.Close()
+		if err != nil {
+			log.Printf("Could not finish writing installer image to temp file")
+			return
+		}
+
+		img, err := walk.NewImageFromFile(tf.Name())
+		if err != nil {
+			log.Printf("Could not load installer image to temp file")
+			return
+		}
+
+		imageView.SetImage(img)
 	}()
 
+	centerWindow(mw)
 	mw.Run()
 }
