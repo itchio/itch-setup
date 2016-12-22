b2c82e573d616a3bf15964075eb81a72231b9bfd
Amos Wenger <amos@itch.io>
2016-12-22 20:23:41 +0100

    don't use gox

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -11,10 +11,6 @@ go version
 export CURRENT_BUILD_PATH=$(pwd)
 export GOPATH=$CURRENT_BUILD_PATH
 export PATH="$PATH:$GOPATH/bin"
-export CGO_ENABLED=1
-
-# set up go cross-compile
-go get github.com/mitchellh/gox
 
 if [ "$CI_OS" = "windows" ]; then
   if [ "$CI_ARCH" = "386" ]; then
@@ -60,7 +56,7 @@ rsync -a --exclude 'src' . src/$PKG || echo "rsync complained (code $?)"
 GOOS=$CI_OS GOARCH=$CI_ARCH go get -v -d -t $PKG
 
 if [ "$CI_OS" = "linux" ]; then
-  export GOX_TAGS="-tags gtk_3_14"
+  export GO_TAGS="-tags gtk_3_14"
 fi
 
 if [ "$CI_OS" = "windows" ]; then
@@ -68,8 +64,11 @@ if [ "$CI_OS" = "windows" ]; then
   file itchSetup.syso
 fi
 
+export GOOS=$CI_OS
+export GOARCH=$CI_ARCH
+
 # compile
-gox -osarch "$CI_OS/$CI_ARCH" -ldflags "$CI_LDFLAGS" -cgo -output="itchSetup" $GOX_TAGS $PKG
+go build -v -x -ldflags "$CI_LDFLAGS" $GO_TAGS $PKG
 
 EXT=""
 if [ "$CI_OS" = "windows" ]; then
