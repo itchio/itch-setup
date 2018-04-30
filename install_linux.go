060dcb27c176e93052a92f83d7e27a5ebcf67e82
Amos Wenger <amoswenger@gmail.com>
2018-04-30 20:20:10 +0200

    linux: use similar strategy as windows for app versions

--- install_linux.go
+++ install_linux.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
+	"strings"
 	"time"
 
 	"io/ioutil"
@@ -15,7 +17,22 @@ import (
 	"github.com/itchio/itch-setup/setup"
 )
 
+var installDir string
+
 func SetupMain() {
+	installDir := filepath.Join(os.Getenv("HOME"), fmt.Sprintf(".%s", appName))
+
+	ids, err := assessInstallDirState(installDir)
+	if err != nil {
+		showError(fmt.Sprintf("%+v", err))
+	}
+
+	if ids.FoundMarker && len(ids.AppDirs) > 0 {
+		log.Printf("Has marker and at least one app dir, trying to launch!")
+		tryLaunch(ids)
+		return
+	}
+
 	gtk.Init(nil)
 
 	imageWidth := 622
@@ -96,7 +113,8 @@ func SetupMain() {
 	// Recursively show all widgets contained in this window.
 	win.ShowAll()
 
-	installDir := filepath.Join(os.Getenv("HOME"), fmt.Sprintf(".%s", appName))
+	versionInstallDir := ""
+	sourceChan := make(chan setup.InstallSource, 1)
 
 	installer := setup.NewInstaller(setup.InstallerSettings{
 		Localizer: localizer,
@@ -116,8 +134,11 @@ func SetupMain() {
 				l.SetText(message)
 			})
 		},
+		OnSource: func(source setup.InstallSource) {
+			sourceChan <- source
+		},
 		OnFinish: func() {
-			itchPath := filepath.Join(installDir, appName)
+			itchPath := filepath.Join(versionInstallDir, exeName())
 			cmd := exec.Command(itchPath)
 			err := cmd.Start()
 			if err != nil {
@@ -131,9 +152,109 @@ func SetupMain() {
 		},
 	})
 
-	installer.Install(installDir)
+	kickoffInstall := func() {
+		kickErr := func() error {
+			err := os.MkdirAll(installDir, 0755)
+			if err != nil {
+				return err
+			}
+
+			source := <-sourceChan
+
+			versionInstallDir = filepath.Join(installDir, fmt.Sprintf("app-%s", source.Version))
+			installer.Install(versionInstallDir)
+
+			return nil
+		}()
+		if kickErr != nil {
+			showError(kickErr.Error())
+		}
+	}
+
+	go kickoffInstall()
 
 	// Begin executing the GTK main loop.  This blocks until
 	// gtk.MainQuit() is run.
 	gtk.Main()
+}
+
+//
+
+type InstallDirState struct {
+	InstallDir    string
+	FoundMarker   bool
+	AppDirs       []string
+	CurrentAppDir string
+}
+
+func assessInstallDirState(installDir string) (*InstallDirState, error) {
+	res := &InstallDirState{
+		InstallDir: installDir,
+	}
+
+	entries, err := ioutil.ReadDir(installDir)
+	if err != nil {
+		log.Printf("Empty (%s), that's ok", installDir)
+		return res, nil
+	}
+
+	log.Printf("Looking through %d entries in %s", len(entries), installDir)
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			if entry.Name() == markerName() {
+				log.Printf("Found marker...")
+				res.FoundMarker = true
+			}
+			continue
+		}
+
+		if !strings.HasPrefix(entry.Name(), "app-") {
+			continue
+		}
+
+		log.Printf("Found app dir %s", entry.Name())
+		res.AppDirs = append(res.AppDirs, entry.Name())
+	}
+
+	if len(res.AppDirs) == 0 {
+		log.Printf("No app dirs in sight, it's install time!")
+		return res, nil
+	}
+
+	log.Printf("Found %d app dirs, sorting them...", len(res.AppDirs))
+	sort.Strings(res.AppDirs)
+
+	// make all paths absolute
+	for i := range res.AppDirs {
+		res.AppDirs[i] = filepath.Join(installDir, res.AppDirs[i])
+	}
+	res.CurrentAppDir = res.AppDirs[len(res.AppDirs)-1]
+
+	return res, nil
+}
+func markerName() string {
+	return fmt.Sprintf(".%s-marker", appName)
+}
+
+func exeName() string {
+	return fmt.Sprintf("%s", appName)
+}
+
+func tryLaunch(ids *InstallDirState) {
+	log.Println("Launching app")
+
+	log.Printf("Current app dir: ", ids.CurrentAppDir)
+	cmd := exec.Command(filepath.Join(ids.CurrentAppDir, exeName()))
+
+	err := cmd.Start()
+	if err != nil {
+		showError(fmt.Sprintf("Encountered a problem while launching %s: %s", appName, err.Error()))
+	}
+
+	log.Printf("App launched, getting out of the way")
+	os.Exit(0)
+}
+
+func showError(msg string) {
+	log.Fatal(msg)
 }
